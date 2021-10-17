2692c1e970e0e086a69b3dbde21ece1613ba2aca
Зотов Николай <zotnik@lan.rarus.ru>
2021-10-17 16:41:18 +0300

    HW5 is completed

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -1,6 +1,7 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"math/rand"
@@ -10,6 +11,11 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
+)
+
+const (
+	TaskDuration = time.Millisecond * 200
+	TaskTickTime = time.Millisecond * 20
 )
 
 func TestRun(t *testing.T) {
@@ -29,7 +35,6 @@ func TestRun(t *testing.T) {
 				return err
 			})
 		}
-
 		workersCount := 10
 		maxErrorsCount := 23
 		err := Run(tasks, workersCount, maxErrorsCount)
@@ -38,7 +43,7 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
-	t.Run("tasks without errors", func(t *testing.T) {
+	t.Run("tasks check concurrency with sleep", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 
@@ -55,7 +60,6 @@ func TestRun(t *testing.T) {
 				return nil
 			})
 		}
-
 		workersCount := 5
 		maxErrorsCount := 1
 
@@ -66,5 +70,103 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("check if maxErrorsCount == 0", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+		workersCount := 7
+		maxErrorsCount := 0
+
+		start := time.Now()
+		err := Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := time.Since(start)
+
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("check if maxErrorsCount == -1", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+		workersCount := 5
+		maxErrorsCount := -1
+
+		start := time.Now()
+		err := Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := time.Since(start)
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("check concurrency using require.Eventually", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var stopTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				curTaskStart := atomic.LoadInt32(&runTasksCount)
+				curTaskStop := atomic.LoadInt32(&stopTasksCount)
+
+				ctx := context.Background()
+				c, cancel := context.WithTimeout(ctx, TaskDuration)
+				defer cancel()
+
+				atomic.AddInt32(&runTasksCount, 1)
+
+				require.Eventually(t, func() bool {
+					if curTaskStart != atomic.LoadInt32(&runTasksCount) ||
+						curTaskStop != atomic.LoadInt32(&stopTasksCount) {
+						cancel()
+						return true
+					}
+					return false
+				}, TaskDuration, TaskTickTime, "eventually")
+
+				<-c.Done()
+				atomic.AddInt32(&stopTasksCount, 1)
+				return nil
+			})
+		}
+		workersCount := 5
+		maxErrorsCount := 1
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 	})
 }
